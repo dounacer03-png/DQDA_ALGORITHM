be7d287b1a3213e3bb8f19b1f45337dba5f5d46e
dounacer03-png <dounacer03@gmail.com>
2025-11-25 11:03:48 +0100

    Create dqda package with __init__.py
    Initialize DQDA package structure and exports

---
+++ dqda/__init__.py
@@ -0,0 +1,12 @@
+"""
+DQDA Algorithm - Dou's Quadratic Distinctiveness Algorithm
+A novel deterministic primality test based on quadratic properties of 6k±1 numbers
+"""
+
+__version__ = "1.0.0"
+__author__ = "DOU ABDENNASSER"
+__email__ = "dounacer03@gmail.com"
+
+from .algorithm import DQDA
+
+__all__ = ["DQDA"]
